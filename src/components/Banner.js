550f468f6c6dec238a6cd85a6476cb3b763938ee
P Sujith Bhatt <sujithdon00797@gmail.com>
2026-01-08 23:12:12 +0530

    Banner Completed

---
+++ src/components/Banner.js
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const toRotate = [
+    "Full-Stack Developer",
+    "Software Engineer",
+    "ThingWorx Developer",
+  ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(200);
+    }
+  };
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>
+              {`Hi! I'm Sujith `}
+              <br></br>
+              <span className="wrap">{text}</span>
+            </h1>
+            <p>
+              Accomplished PTC ThingWorx Developer with over 5 years of
+              experience, highly proficient in Node.js, Express.js, MongoDB, &
+              React.js. Seeking to completely transition into a full-stack
+              development role to embrace new challenges & expand technical
+              expertise. Eager to join an innovative organization that offers
+              growth opportunities & empowers individual creativity &
+              development.
+            </p>
+            <button onClick={() => console.log("connect")}>
+              Let's Connect <ArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Banner" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
